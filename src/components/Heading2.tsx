f23b1b8578a22f57c144f399578fcebc7447c9c5
MACOPOC <macopoc.bess@gmail.com>
2024-12-02 22:23:54 +0900

    빌드 버그 수정 : children 프로퍼티에 타입 지정

--- src/components/Heading2.tsx
+++ src/components/Heading2.tsx
@@ -1,4 +1,6 @@
-export default function Heading2({ children }) {
+import { ReactNode } from "react";
+
+export default function Heading2({ children }: { children: ReactNode }) {
   return (
     <h2 className="mb-4 text-l leading-none tracking-tight md:text-xl lg:text-2xl dark:text-white">
       {children}
